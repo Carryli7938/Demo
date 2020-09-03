8581da7d19c4ebadb940239c89a3d07092cf0408
lida <li.da@immomo.com>
2020-09-03 16:14:53 +0800

    session01

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+
     @CrossOrigin
-    @GetMapping(value = "/hello")
+    @GetMapping(value = "/hello") 
+    
     // @ResponseBody
-    public String hello(){
-        return "Hello World lida";
+    public String hello(HttpServletRequest request,HttpSession sesssion){
+        String str = new String();
+        str = str + "loginSessionId:" + request.getSession().getId() + ";";
+        str = str + "getRequestURL:" + request.getRequestURL() + ";";
+        str = str + "getServerPort:" + request.getServerPort() + ";";
+        str = str + "getProtocol:" + request.getProtocol() + ";";
+
+        str = str + "getRemoteAddr:" + request.getRemoteAddr() + ";";
+        str = str + "getRemoteHost:" + request.getRemoteHost() + ";";
+        str = str + "getRemotePort:" + request.getRemotePort() + ";";
+        str = str + "getLocalAddr:" + request.getLocalAddr() + ";";
+        str = str + "getLocalName:" + request.getLocalName() + ";";
+        str = str + "getLocalPort:" + request.getLocalPort() + ";";   
+        return "Hello World lida"+str;
     }
+    
 
 }
